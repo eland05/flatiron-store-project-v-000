c5a9328b8b96055745f83f0845fe934cc492fcc9
Erin Land <erinland05@gmail.com>
2018-02-13 15:47:25 +0000

    Done.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,13 @@
 class CartsController < ApplicationController
+  def show
+   @cart = Cart.find(params[:id])
+ end
 
+ def checkout
+   cart = Cart.find params[:id]
+   cart.checkout
+   cart.save
+   current_user.remove_cart
+   redirect_to cart_path
+ end
 end
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,3 +1,14 @@
 class LineItemsController < ApplicationController
-
+  def create
+    if current_user
+      if current_user.current_cart.nil?
+        current_user.current_cart = Cart.create(user_id: current_user.id)
+        current_user.save
+      end
+      current_user.current_cart.add_item(params[:item_id]).save
+      redirect_to cart_path(current_user.current_cart), {notice: 'Item added to cart!'}
+    else
+      redirect_to store_path, {notice: 'Unable to add item'}
+    end
+  end
 end
